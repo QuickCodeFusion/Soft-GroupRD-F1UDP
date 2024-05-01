2b603209d90751146452b1c7894901bcf678b3ba
Uriel Campos <urielcampos448@gmail.com>
2024-05-01 11:02:47 -0300

    feat[listeners/io] added new ParticipantsListener

--- src/listeners/io/Participants.ts
+++ src/listeners/io/Participants.ts
@@ -1,10 +1,35 @@
 import EnvVars from '@src/constants/EnvVars';
 import { NodeEnvs } from '@src/constants/misc';
-import { PacketParticipantsData } from 'f1-23-udp';
+import { PacketParticipantsData, ParticipantData } from 'f1-23-udp';
 import { Socket } from 'socket.io';
+import { NormalizedParticipantData, NormalizedParticipantsData } from './types/F123UDP';
 
-export const ParticipantsListener = (io: Socket, data: PacketParticipantsData) => {
-  io.emit('Participants', data);
+export const ParticipantsListener = (
+  io: Socket,
+  data: PacketParticipantsData | undefined) => {
+  if (data) {
+    const normalizedData: NormalizedParticipantsData = {
+      header: data.m_header,
+      numParticipants: data.m_numActiveCars,
+      participants: data.m_participants.map((participant: ParticipantData) => {
+        return {
+          aiControlled: participant.m_aiControlled,
+          driverId: participant.m_driverId,
+          networkId: participant.m_networkId,
+          teamId: participant.m_teamId,
+          myTeam: participant.m_myTeam,
+          raceNumber: participant.m_raceNumber,
+          nationality: participant.m_nationalty,
+          name: participant.m_name,
+          platform: participant.m_platform,
+        } as NormalizedParticipantData;
+      }),
+    };
+    io.emit('Participants', normalizedData);
+  }
+  else {
+    throw new Error('Invalid data: ' + data);
+  }
   if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
     // eslint-disable-next-line no-console
     console.log('Participants', data);
---
+++ src/listeners/io/types/F123UDP.d.ts
@@ -0,0 +1,30 @@
+import {
+  type PacketLapData,
+  type PacketParticipantsData,
+  type ParticipantData,
+} from 'f1-23-udp';
+
+export type NormalizedParticipantData = {
+  aiControlled: ParticipantData['m_aiControlled']
+  driverId: ParticipantData['m_driverId']
+  networkId: ParticipantData['m_networkId']
+  teamId: ParticipantData['m_teamId']
+  myTeam: ParticipantData['m_myTeam']
+  raceNumber: ParticipantData['m_raceNumber']
+  nationality: ParticipantData['m_nationalty']
+  name: ParticipantData['m_name']
+  platform: ParticipantData['m_platform']
+}
+
+export type NormalizedParticipantsData = {
+    header: PacketParticipantsData['m_header']
+    numParticipants: PacketParticipantsData['m_numActiveCars']
+    participants: NormalizedParticipantData[]
+}
+
+export type NormalizedParticipantsData = {
+    header: PacketLapData['m_header']
+    lapData: PacketLapData['m_lapData']
+    timeTrialPBCar: PacketLapData['m_timeTrialPBCarIdx']
+    timeTrialRivalCar: PacketLapData['m_timeTrialRiva']
+}
