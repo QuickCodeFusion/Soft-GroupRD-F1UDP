cf32428b4746eb0b0cf9622abb03da975140cd96
Uriel <urielcampos448@gmail.com>
2024-05-06 11:40:10 -0300

    added telemetry nextjs app

---
+++ telemetry-app/app/page.tsx
@@ -0,0 +1,70 @@
+"use client"
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+interface Driver {
+  aiControlled: number
+  driverId: number
+  networkId: number
+  teamId: number
+  myTeam: number
+  raceNumber: number
+  nationality: number
+  name: string
+  platform: number
+  position: number
+}
+
+const RaceDriversTable: React.FC = () => {
+  const [drivers, setDrivers] = useState<Driver[]>([]);
+
+  useEffect(() => {
+    const socket = io('ws://localhost:3000', {
+      
+    });
+
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from Socket.IO server');
+    });
+
+    socket.on('participantsPosition', (data: Driver[]) => {
+      setDrivers(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Race Drivers</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Car Number</th>
+            <th>Nationality</th>
+            <th>Position</th>
+          </tr>
+        </thead>
+        <tbody>
+          {drivers.map((driver, index) => (
+            <tr key={index}>
+              <td>{driver.name}</td>
+              <td>{driver.raceNumber}</td>
+              <td>{driver.nationality}</td>
+              <td>{driver.position}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RaceDriversTable;
