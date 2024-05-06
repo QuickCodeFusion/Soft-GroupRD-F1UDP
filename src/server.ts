16efd64148f4d843474c17cea52593af97946f06
Uriel <urielcampos448@gmail.com>
2024-05-06 11:50:10 -0300

    feat[server] added cors

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -29,6 +30,7 @@ const app = express();
 // Basic middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 // Show routes called in console during development
 if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
