65fa22126d8bbc9f7151e10cfd745a46152e6bdb
Uriel <urielcampos448@gmail.com>
2024-05-06 10:49:22 -0300

    feat[util/raceData] relateLapDataToDriver now returns driversData array with their position

--- src/util/raceData.ts
+++ src/util/raceData.ts
@@ -1,31 +1,56 @@
-import { PacketLapData, PacketParticipantsData } from "f1-23-udp";
+import { NormalizedParticipantData } from '@src/listeners/io/types/F123UDP';
+import { PacketLapData, PacketParticipantsData } from 'f1-23-udp';
 
-export function relateLapDataToDriver(lapDataPacket: PacketLapData, participantsPacket: PacketParticipantsData): void {
-    const driverData: { [driverId: number]: string } = {};
-
-    // Extract driver data from participants packet
-    participantsPacket.m_participants.forEach(participant => {
-        if(participant.m_aiControlled === 1) return;
-        const driverId = participant.m_networkId;
-        const driverName = participant.m_showOnlineNames === 0 ? 'Unknown' : participant.m_name;
-        driverData[driverId] = driverName;
-    });
-
-    // Associate driver names with lap data
-    lapDataPacket.m_lapData.forEach((lapData, index) => {
-        const carPosition = lapData.m_carPosition;
-        
-        if (carPosition > 0 && carPosition <= participantsPacket.m_participants.length) {
-            const driverId = participantsPacket.m_participants[carPosition - 1].m_networkId;
-            
-            const driverName = driverData[driverId];
-
-            if (driverName) {
-                console.log(`Driver: ${driverId}, Lap Position: ${index + 1}`);
-            }
-        }
-    });
-
-    
-    
+interface ParticipantPosition extends NormalizedParticipantData {
+    position: number;
 }
+
+const relateLapDataToDriver = (
+  lapDataPacket: PacketLapData,
+  participantsPacket: PacketParticipantsData) => {
+  const driversData: ParticipantPosition[] = [];
+
+  const driversDictionary: Record<number, string> = {};
+
+  // Extract driver data from participants packet
+  participantsPacket.m_participants.forEach(participant => {
+    if(participant.m_aiControlled === 1) return;
+    const driverId = participant.m_networkId;
+    const driverName = participant.m_showOnlineNames ? 
+      participant.m_name 
+      : `Jugador ${participant.m_networkId}`;
+    driversDictionary[driverId] = driverName;
+  });
+
+  // Associate driver names with lap data
+  lapDataPacket.m_lapData.forEach((lapData) => {
+    const carPosition = lapData.m_carPosition;
+        
+    if (carPosition > 0 && carPosition <= participantsPacket.m_participants.length) {
+      const driver = participantsPacket.m_participants[carPosition - 1];
+            
+      const driverName = driversDictionary[driver.m_driverId];
+
+      if (driverName) {
+        // eslint-disable-next-line no-console
+        console.log(`Driver: ${driver.m_driverId}, Lap Position: ${carPosition}`);
+        driversData.push({
+          name: driverName,
+          aiControlled: driver.m_aiControlled,
+          driverId: driver.m_driverId,
+          networkId: driver.m_networkId,
+          myTeam: driver.m_myTeam,
+          raceNumber: driver.m_raceNumber,
+          platform: driver.m_platform,
+          position: carPosition,
+          teamId: driver.m_teamId,
+          nationality: driver.m_nationalty,
+        });
+      }
+    }
+  }); 
+
+  return driversData;
+};
+
+export { relateLapDataToDriver };
