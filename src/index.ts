202a25cf1996b2fbc582160dbf1fef6b5fbd0c7e
Uriel <urielcampos448@gmail.com>
2024-05-08 17:54:10 -0300

    feat[index] now using Origins envvar instead of hardcoded strings

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001'],
+    origin: EnvVars.Origins,
   },
 });
 
