850dfe7e54474b7d913ec48f678e60e942ee539a
juanpo12 <juanjoc2000@hotmail.com>
2024-05-10 17:23:01 -0300

    fix racedata conditional

--- src/util/raceData.ts
+++ src/util/raceData.ts
@@ -11,7 +11,9 @@ const relateLapDataToDriver = (
   const driversData: ParticipantPosition[] = [];
 
   const driversDictionary: Record<number, string> = {};
-
+  
+  if(!lapDataPacket || !participantsPacket) return;
+  
   // Extract driver data from participants packet
   participantsPacket.m_participants.forEach(participant => {
     if(participant.m_aiControlled === 1) return;
@@ -19,18 +21,19 @@ const relateLapDataToDriver = (
     console.log('Driver ID:', driverId);
     
     const driverName = participant.m_showOnlineNames ? 
-      participant.m_name 
-      : `Jugador ${driverId !== 255 ? driverId : 'IA'}`;
+    participant.m_name 
+    : `Jugador ${driverId !== 255 ? driverId : 'IA'}`;
     driversDictionary[driverId] = driverName;
   });
 
   // Associate driver names with lap data
-  lapDataPacket.m_lapData.forEach((lapData) => {
+  lapDataPacket?.m_lapData?.forEach((lapData) => {
     const carPosition = lapData.m_carPosition;
-        
-    if (carPosition > 0 && carPosition <= participantsPacket.m_participants.length) {
-      const driver = participantsPacket.m_participants[carPosition - 1];
-            
+
+    if(carPosition < 1 || carPosition > 20) return;
+    
+    const driver = participantsPacket.m_participants[carPosition - 1];
+
       const driverName = driversDictionary[driver.m_networkId];
 
       if (driverName) {
@@ -48,8 +51,9 @@ const relateLapDataToDriver = (
           nationality: driver.m_nationalty,
         });
       }
+
     }
-  }); 
+  ); 
 
   return driversData;
 };
